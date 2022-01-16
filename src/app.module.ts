a2c92d2ca6b85833df036c98a083087d3b57fab4
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2022-01-16 18:06:50 +0500

    Updated

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://shahzaibnoor:zx112233445566@cluster0.vhrvh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    ),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
