e312ef126cf7f4f73282256eb126ee3c8b460d64
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2022-01-19 18:00:07 +0500

    Updated

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserService } from './user.service';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [
@@ -7,5 +9,7 @@ import { MongooseModule } from '@nestjs/mongoose';
       'mongodb+srv://shahzaibnoor:zx112233445566@cluster0.wpuzy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     ),
   ],
+  providers: [UserService],
+  controllers: [UserController],
 })
 export class UserModule {}
