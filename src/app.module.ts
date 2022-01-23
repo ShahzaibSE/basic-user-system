c47f4a57b14346f52da9daff41aee6de2357fa19
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2022-01-23 15:53:05 +0500

    Updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { MongooseModule } from '@nestjs/mongoose';
     UserModule,
     AuthModule,
     MongooseModule.forRoot(
-      'mongodb+srv://shahzaibnoor:zx112233445566@cluster0.wpuzy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+      'mongodb+srv://shahzaibnoor:zx112233445566@cluster0.hz0mu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     ),
   ],
   controllers: [AppController],
