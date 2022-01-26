b6835e6bd693e21edc0fc32961ff090cb5869987
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2022-01-26 15:38:39 +0500

    Updated

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 // Service.
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly service: UserService) {}
+  //
+  @UseGuards(AuthGuard('local'))
+  @Post('login')
+  async login(@Request() req) {
+    return req.user;
+  }
 }
