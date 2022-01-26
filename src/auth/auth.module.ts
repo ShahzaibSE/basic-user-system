401059aec098878429ff045badba489c6606a400
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2022-01-26 14:11:44 +0500

    Updated

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserModule } from './../user/user.module';
 import { LocalStrategy } from './local.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, PassportModule],
   providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
