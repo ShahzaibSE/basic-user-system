3fdaf55b2f49edce51389afe6f3d515788db6cb0
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2022-01-26 20:50:33 +0500

    Updated

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(cookieParser());
+  //
+  const config = new DocumentBuilder()
+    .setTitle('Basic User System')
+    .setDescription('Basic User System API')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  //
   await app.listen(3000);
 }
 bootstrap();
