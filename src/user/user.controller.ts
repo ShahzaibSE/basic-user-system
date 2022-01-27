1aaa9a16225a3dce4a2a5171d26fb6fe3cf15bdc
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2022-01-27 14:34:29 +0500

    Updated

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,4 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
-import { LocalAuthGuard } from './../auth/local-auth.guard';
-// Service.
-import { UserService } from './user.service';
-import { AuthService } from './../auth/auth.service';
+import { Controller } from '@nestjs/common';
 
 @Controller('user')
 export class UserController {}
